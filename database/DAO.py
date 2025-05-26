113a0cf48027967f217e9ce3f395204bea949d0f
SavaNicolas <s311650@studenti.polito.it>
2025-05-26 11:31:52 +0200

    parte 1

--- database/DAO.py
+++ database/DAO.py
@@ -1,6 +1,74 @@
 from database.DB_connect import DBConnect
+from model.album import Album
 
 
 class DAO():
-    def __init__(self):
-        pass
+    @staticmethod
+    def getAllAlbums():
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """
+            select a.*, sum(t.Milliseconds)/1000/60 as dTot
+from album a, track t
+where a.AlbumId = t.AlbumId
+group by a.AlbumId
+    """
+        cursor.execute(query)
+
+        for row in cursor:
+            result.append(Album(**row))
+            # equivalente a fare (ArtObject(object_id= row["object_id"])
+        cursor.close()
+        conn.close()
+        return result  # lista di nazioni
+
+    @staticmethod
+    def getNodes(dMin):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """
+            select a.*, sum(t.Milliseconds)/1000/60 as dTot
+from album a, track t
+where a.AlbumId = t.AlbumId
+group by a.AlbumId
+having dTot > %s
+    """
+        cursor.execute(query)
+
+        for row in cursor:
+            result.append(Album(**row))
+            # equivalente a fare (ArtObject(object_id= row["object_id"])
+        cursor.close()
+        conn.close()
+        return result  # lista di nazioni
+
+    @staticmethod
+    def getAllEdges(idMap):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """
+                select distinct t1.AlbumId,t2.AlbumId
+from track t1, track t2, playlisttrack p1,playlisttrack p2
+where t2.TrackId =p2.TrackId
+and t1.TrackId =p1.TrackId
+and p2.PlaylistId =p1.PlaylistId
+and t1.AlbumId <t2.AlbumId
+
+        """
+        cursor.execute(query)
+
+        for row in cursor:
+            result.append((idMap[row["a1"]], idMap[row["a2"]])) #arco come tupla
+            # equivalente a fare (ArtObject(object_id= row["object_id"])
+        cursor.close()
+        conn.close()
+        return result  # lista di nazioni
--- model/model.py
+++ model/model.py
@@ -1,3 +1,48 @@
+import networkx as nx
+
+from database.DAO import DAO
+
+
 class Model:
     def __init__(self):
-        pass
+        self._albumsAll = DAO.getAllAlbums() # lista con tutti i retailer
+        # creo grafo
+        self._grafo = nx.Graph()  # semplice, non orientato e pesato
+        # mappa di oggetti
+        self.idAlbums = {}
+        for p in self._albumsAll:
+            self.idAlbums[p.AlbumId] = p
+
+        self._nodi= None
+
+    def buildGraph(self,durataMin,anno):
+        self._grafo.clear()
+        self._nodi = self.getNodes(durataMin)  # retailer presi in base alla nazionalità
+        # aggiungiamo i nodi(li ho nelle fermate)
+        self._grafo.add_nodes_from(self._nodi)
+        # aggiungo archi
+        self.addEdges(anno,self.idAlbums)
+
+    def addEdges(self,anno,idMap):
+        """
+        Due album a1 e a2 sono collegati tra loro se
+almeno una canzone di a1 e una canzone di a2
+sono state inserite da un utente all’interno di
+una stessa playlist (tabella PlaylistTrack).
+        """
+        edges= DAO.getAllEdges(idMap)
+        self._grafo.add_edges_from(edges)
+
+#metodi che devono esserci sempre
+    def getNumNodi(self):
+        return len(self._grafo.nodes())
+
+    def getNumArchi(self):
+        return len(self._grafo.edges())
+
+    def getNodi(self):
+        return self._nodi
+        #return list(self._grafo.nodes())
+
+    def getGraph(self):
+        return self._grafo
